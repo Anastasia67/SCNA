cae8cf093ce0e8c6393bfd10a02d91694b21d6f1
Anastasia Kharchenko <anastasia.falls@gmail.com>
2024-10-02 10:35:27 +0200

    npm install react-native-msal

--- frontend/SCNA/App.js
+++ frontend/SCNA/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import HomeScreen from "./HomeScreen"; // Importeer de voorpagina component
-import LoginScreen from "./LoginScreen"; // Zorg ervoor dat je deze import hebt
+import HomeScreen from "./HomeScreen"; 
+import LoginScreen from "./LoginScreen"; 
 
 const Stack = createNativeStackNavigator();
 
