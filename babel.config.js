8f6ee34744202d5fcf03251c2be5f1c837dbda52
Anastasia67 <anastasia.falls@gmail.com>
2024-12-11 11:27:54 +0100

    config

---
+++ babel.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    preset: 'module:metro-react-native-babel-preset', // Belangrijk voor React Native,
+    rootDir: './SCNA', // Pas dit aan naar de map waar je tests en code staan
+  };
---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    preset: 'react-native',
+    rootDir: './SCNA', // Pas dit aan naar de map waar je tests en code staan
+    transform: {
+      '^.+\.[jt]sx?$': 'babel-jest', // Gebruik babel-jest om JavaScript en TypeScript te transpilen
+    },
+    collectCoverage: true,
+    coverageDirectory: '../coverage', // Zorg dat coverage buiten rootDir wordt opgeslagen
+    coverageReporters: ['lcov', 'text'],
+    testEnvironment: 'jsdom',
+    collectCoverageFrom: [
+      '/*.{js,jsx,ts,tsx}', // Alle JavaScript en TypeScript bestanden in de map
+      '!/nodemodules/', // Sluit node_modules uit
+      '!/coverage/',     // Sluit de coverage map uit
+    ],
+    testMatch: [
+      '/tests/*/.test.js', // Zoek tests in _tests mappen
+      '*/?(.)+(spec|test).[jt]s?(x)', // Zoek tests met .test.js of .spec.js extensie
+    ],
+  };
